9a4ea923b9131d044a4f184bcabb628be361a907
mohammad ghane <mghaneh1375@yahoo.com>
2022-04-28 19:06:59 +0430

    redirect / to /en

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return Redirect::to('/en');
+});
+
 Route::prefix('{locale}')->middleware('checkLang')->group(function () {
 
     Route::get('/', function () {
         return view('welcome');
-    });
+    })->name('home');
 
 });
 
