2bb61f2a4ee134052c2ccc70eb214932f00152df
mohammad ghane <mghaneh1375@yahoo.com>
2022-05-23 11:44:37 +0430

    filter added.

--- resources/views/layouts/filter.blade.php
+++ resources/views/layouts/filter.blade.php
@@ -3,7 +3,10 @@
 <div id="filter-box">
 
 <ul>
-    <li>Select Category</li>
+    <li id="selectedItem">Select Category</li>
+
+    <div id="choices" class="close"></div>
+
     <img id="drop-down-arrow" src="{{asset('assets/images/chevron-down.svg')}}">
 </ul>
 
@@ -17,6 +20,23 @@
 
     var onlyVideo = false;
 
+    function filter(cat) {
+
+        $("#choices").toggleClass('close');
+
+        if(onlyVideo)
+            return;
+
+        $("#selectedItem").empty().append(cat);
+
+        cat = "_" + cat +"_";
+
+        $("#all .item").addClass('hidden').each(function () {
+            if($(this).attr('data-cats').includes(cat))
+                $(this).removeClass('hidden');
+        });
+    }
+
     $(document).ready(function () {
 
         $("#back").on('click', function () {
@@ -27,12 +47,23 @@
 
             onlyVideo = !onlyVideo;
 
-            if(onlyVideo)
-                $(".item").addClass('hidden');
-            else
-                $(".item").removeClass('hidden');
+            if(onlyVideo) {
+                $("#all .item").addClass('hidden');
+                $("#videoFilter").addClass('selected');
+            }
+            else {
+                $("#all .item").removeClass('hidden');
+                $("#videoFilter").removeClass('selected');
+            }
         });
 
+        $("#drop-down-arrow").on('click', function () {
+            $("#choices").toggleClass('close');
+        });
+
+        $("#selectedItem").on('click', function () {
+            $("#choices").toggleClass('close');
+        });
     });
 
 </script>
